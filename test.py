8ab0f01419bb7e5c0c08968b2ff132c5550be74d
lolmaomg <alex6010111@gmail.com>
2026-09-18 01:20:18 -0500

    Commit  message for y

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ a = np.array([1, 2, 3, 4])           # 从列表创建
 b = np.zeros((2, 3))                 # 2行3列，全是0
 c = np.arange(0, 10, 2)              # 0到10，步长2 → [0,2,4,6,8]
 d = np.linspace(0, 1, 5)             # 0到1之间，均匀取5个数
-
-
+e = np.random.rand(2, 3)             # 2行3列，随机数
 np.random.seed(111231234)
 print(np.random.rand(2,3))          # 3行2列，随机数
